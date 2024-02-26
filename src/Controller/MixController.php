cfaa1761cc6722962c7b2d536864ba4049f61d2e
tsydenov <tsydenovs@mail.ru>
2024-02-26 23:20:30 +0700

    Adding new feature vote

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,7 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,13 +34,31 @@ class MixController extends AbstractController
         ));
     }
 
-    #[Route('/mix/{id}')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
+        ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(
+        VinylMix $mix,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $direction = $request->request->get('direction', 'up');
+
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_mix_show', [
+            'id' => $mix->getId(),
         ]);
     }
 }
