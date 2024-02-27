e1528f58e26a76f413dc20ce773b7167eefdd4bc
tsydenov <tsydenovs@mail.ru>
2024-02-27 16:15:30 +0700

    Adding sluggable to VinylMix

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -34,7 +34,7 @@ class MixController extends AbstractController
         ));
     }
 
-    #[Route('/mix/{id}', name: 'app_mix_show')]
+    #[Route('/mix/{slug}', name: 'app_mix_show')]
     public function show(VinylMix $mix): Response
     {
         return $this->render('mix/show.html.twig', [
@@ -59,7 +59,7 @@ class MixController extends AbstractController
         $this->addFlash('success', 'Vote counted!');
 
         return $this->redirectToRoute('app_mix_show', [
-            'id' => $mix->getId(),
+            'slug' => $mix->getSlug(),
         ]);
     }
 }
--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\VinylMixRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Slug;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: VinylMixRepository::class)]
@@ -32,6 +33,10 @@ class VinylMix
 
     #[ORM\Column]
     private int $votes = 0;
+
+    #[ORM\Column(length: 100, unique: true)]
+    #[Slug(fields: ['title'])]
+    private ?string $slug = null;
 
     public function getId(): ?int
     {
@@ -119,5 +124,17 @@ class VinylMix
     public function downVote(): void
     {
         $this->votes--;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
     }
 }
